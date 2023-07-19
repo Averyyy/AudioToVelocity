ee2eebb861c3a070f49a63f312c8ce8a86aafe6a
Avery <hq443@nyu.edu>
2023-07-19 16:39:02 +0800

    polished train.py

--- src/train.py
+++ src/train.py
@@ -15,24 +15,20 @@ def train(model, dataloader, criterion, optimizer, device, scheduler):
     total_loss = 0.0
 
     for i, (audio, midi, velocity) in enumerate(dataloader):
-        # Move data to the right device
         audio = audio.to(device)
         midi = midi.to(device)
         velocity = velocity.to(device)
 
-        # Forward pass
         output = model(audio, midi)
-        # print(output.shape, velocity.shape)
         loss = criterion(output, velocity)
 
-        # Backward pass and optimization
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         scheduler.step()
-        scheduler.step()
 
         total_loss += loss.item()
+
         if i % 100 == 0:
             print(f'Batch {i} Loss: {loss.item()}')
 
@@ -46,12 +42,10 @@ def validate(model, dataloader, criterion, device):
 
     with torch.no_grad():
         for i, (audio, midi, velocity) in enumerate(dataloader):
-            # Move data to the right device
             audio = audio.to(device)
             midi = midi.to(device)
             velocity = velocity.to(device)
 
-            # Forward pass and calculate loss
             output = model(audio, midi)
             loss = criterion(output, velocity)
 
@@ -62,17 +56,8 @@ def validate(model, dataloader, criterion, device):
 
 
 def main():
-    # Set device configuration
-    if torch.backends.mps.is_available():
-        device = torch.device('mps')
-    else:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    if torch.backends.mps.is_available():
-        device = torch.device('mps')
-    else:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Define hyperparameters
     hidden_dim = 512
     num_layers = 32
     nhead = 16
@@ -80,72 +65,59 @@ def main():
     num_epochs = 50
     batch_size = 4
 
-    # Load and split dataset
     print('----- Loading Dataset -----')
+    data_dir = os.path.join('data', 'SMD-8s-normalize')
+
     if not os.path.exists('data/processed/train_data.pkl'):
-        data_dir = os.path.join('data', 'SMD-8s-normalize')
         dataset = VelocityDataset(data_dir)
-        # print(len(dataset))
         train_data, val_data = train_test_split(
             dataset, test_size=0.2, random_state=42)
+
         if not os.path.exists('data/processed'):
             os.makedirs('data/processed')
+
         with open('data/processed/train_data.pkl', 'wb') as f:
             pickle.dump(train_data, f)
+
         with open('data/processed/val_data.pkl', 'wb') as f:
             pickle.dump(val_data, f)
+
     else:
         with open('data/processed/train_data.pkl', 'rb') as f:
             train_data = pickle.load(f)
+
         with open('data/processed/val_data.pkl', 'rb') as f:
             val_data = pickle.load(f)
 
-    # Define dataloaders
     train_dataloader = DataLoader(
         train_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
     val_dataloader = DataLoader(
         val_data, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
 
-    # Define the model, loss function, and optimizer
     model = TransformerModel(freq_dim=1025, note_dim=90, hidden_dim=hidden_dim,
                              nhead=nhead, num_layers=num_layers).to(device)
     criterion = nn.CrossEntropyLoss()
-    criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    # Decays learning rate by a factor of 0.1 every 10 epochs
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
-
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-    # Decays learning rate by a factor of 0.1 every 10 epochs
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
     if not os.path.exists('logs'):
         os.makedirs('logs')
 
     print('----- Start Training -----')
-    # Training loop
-    for epoch in range(num_epochs):
-        train_loss = train(model, train_dataloader,
-                           criterion, optimizer, device, scheduler)
-        val_loss = validate(model, val_dataloader, criterion, device)
-        print(
-            f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}, Validation Loss: {val_loss}')
-        with open('logs/train_loss.txt', 'w') as f:
+    with open('logs/train_loss.txt', 'w') as f:
+        for epoch in range(num_epochs):
+            train_loss = train(model, train_dataloader,
+                               criterion, optimizer, device, scheduler)
+            val_loss = validate(model, val_dataloader, criterion, device)
+
+            print(
+                f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}, Validation Loss: {val_loss}')
             f.write(
                 f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}, Validation Loss: {val_loss}\n')
 
-    # Save the model
     if not os.path.exists('checkpoints'):
         os.makedirs('checkpoints')
-    torch.save(model.state_dict(), 'checkpoints/model.pth')
-    with open('logs/train_loss.txt', 'a') as f:
-        f.write(
-            f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}, Validation Loss: {val_loss}\n')
 
-    # Save the model
-    if not os.path.exists('checkpoints'):
-        os.makedirs('checkpoints')
     print('----- Saving Model -----')
     torch.save(model.state_dict(), 'checkpoints/model.pth')
 
